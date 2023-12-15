705ccb80af84b2bc6bdc2fe0efe8c9492d9bc46a
arrooxa <cambui01_@hotmail.com>
2023-12-14 23:58:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+const { OpenAI } = require ('openai')
+
+const openai = new OpenAI(OPENAI_API_KEY)
+
+const express = require('express')
+const app = express()
+
+const PORT = 4000
+
+app.use(express.json())
+
+function defineGptSettings(prompt){
+    return {
+        messages: [{ role: 'user', content: prompt}],
+        model: 'gpt-3.5-turbo',
+        max_tokens: 40
+    }
+};
+
+app.post('/pergunte-ao-chatgpt', async (req, res) => {
+    const { prompt } = req.body
+    
+    const completion = await openai.chat.completions.create(defineGptSettings(prompt));
+
+    res.json({completion: completion.choices[0].message.content})
+})
+
+app.listen(PORT, () => console.log(`Em execução na porta ${PORT}.`))
